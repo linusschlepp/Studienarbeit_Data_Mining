1cd282b5c3ff207837dca2ca40aa62180772633d
linusschlepp <linusmo.94@googlemail.com>
2023-01-07 14:21:54 +0100

    Continued working

--- Listing/neuronal_network.py
+++ Listing/neuronal_network.py
@@ -2,10 +2,10 @@ model = Sequential()
 
 X, y = utils.prepare_for_tensor(X_data, target)
 
+# 9 features = 9 input nodes 
+model.add(Dense(9, input_dim=X_data.shape[1], activation='sigmoid', name='input'))
 
-model.add(Dense(14, input_dim=X_data.shape[1], activation='sigmoid', name='input'))
-
-
+# 15 nodes for the hidden layers
 model.add(Dense(15, activation='relu', name='layer1'))
 model.add(Dense(15, activation='relu', name='layer2'))
 model.add(Dense(15, activation='relu', name='layer2'))
