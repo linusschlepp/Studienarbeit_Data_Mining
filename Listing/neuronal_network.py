5872900c4c5155134a9cd00565f3c0188fc90acf
linusschlepp <linusmo.94@googlemail.com>
2023-01-09 21:34:16 +0100

    Did some changes

--- Listing/neuronal_network.py
+++ Listing/neuronal_network.py
@@ -8,7 +8,7 @@ model.add(Dense(9, input_dim=X_data.shape[1], activation='sigmoid', name='input'
 # 15 nodes for the hidden layers
 model.add(Dense(15, activation='relu', name='layer1'))
 model.add(Dense(15, activation='relu', name='layer2'))
-model.add(Dense(15, activation='relu', name='layer2'))
+model.add(Dense(15, activation='relu', name='layer3'))
 model.add(Dense(1, activation='sigmoid', name='output'))
 
 model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
