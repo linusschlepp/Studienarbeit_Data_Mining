f4f68892a458bf1029dd9942ac055dc505a9eb1c
linusschlepp <linusmo.94@googlemail.com>
2023-01-14 20:37:00 +0100

    Final commit

--- Listing/neuronal_network.py
+++ Listing/neuronal_network.py
@@ -2,13 +2,13 @@ model = Sequential()
 
 X, y = utils.prepare_for_tensor(X_data, target)
 
-# 9 features = 9 input nodes 
-model.add(Dense(9, input_dim=X_data.shape[1], activation='sigmoid', name='input'))
+# 12 input nodes
+model.add(Dense(12, input_dim=X_data.shape[1], activation='sigmoid', name='input'))
 
-# 15 nodes for the hidden layers
-model.add(Dense(15, activation='relu', name='layer1'))
-model.add(Dense(15, activation='relu', name='layer2'))
-model.add(Dense(15, activation='relu', name='layer3'))
+# 20 nodes for the hidden layers
+model.add(Dense(20, activation='relu', name='layer1'))
+model.add(Dense(20, activation='relu', name='layer2'))
+model.add(Dense(20, activation='relu', name='layer3'))
 model.add(Dense(1, activation='sigmoid', name='output'))
 
 model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 Im Rahmen des Wahlfaches "Data Mining" galt es, einen Datensatz zu analysieren. Ich habe
 mich dabei für einen Datensatz über die Krankheit Affenpocken entschieden. Der Datensatz findet sich 
 [hier](https://www.kaggle.com/datasets/muhammad4hmed/monkeypox-patients-dataset).
-Binär-Klassifikation, stellt dabei die verwendete Anwendung dar. 
+Binär-Klassifikation stellt dabei die verwendete Anwendung dar. 
 
 Die Berechnungen zu dieser Untersuchung, finden sich in dieser [Repository](https://github.com/linusschlepp/Data-Mining-Project). 
